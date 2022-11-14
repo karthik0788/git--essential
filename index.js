e30d11e5578312a99f98ed43d113b71eed064f28
karthik0788 <karthikgouda941@gmail.com>
2022-11-14 12:11:03 -0800

    this is my commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log("hello");
+//var a=10;
+//var b=20;
+//sum=a+b;
+//console.log(sum)
+//console.log(a==b)
+myObj.test="test"
+myObj={first:"karthik",last:"gouda"
+
+}
+myObj.first="john";
+console.log(myObj)
