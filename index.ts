90d49e05522e42231fe0ebc4518129fc9355130c
karthik0788 <karthikgouda941@gmail.com>
2022-11-16 23:17:11 -0800

    first repo

---
+++ index.ts
@@ -0,0 +1,7 @@
+//function calculateTotalPrice(quantity, unitPrice) { 
+//	return quantity * unitPrice;
+ //
+//}
+//console.log(calculateTotalPrice(3, "500"));
+let message:string="hello world"
+console.log(message);
---
+++ twit.js
@@ -0,0 +1,14 @@
+//myArray [4]=[hello, guard,];
+//console.log(4);
+let x =500;
+console.log(x+x);
+console.log("hello+world");
+
+console.log(y);
+
+
+let a,b,c;
+a=5;
+b=c=a+5;
+
+
