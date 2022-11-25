3c3d8317b0578205dd430d37462e5847ca91754f
karthik0788 <karthikgouda941@gmail.com>
2022-11-24 17:30:55 -0800

    this a index js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-//console.log("hello");
-//var a=10;
-//var b=20;
-//sum=a+b;
-//console.log(sum)
-//console.log(a==b)
-myObj.test="test"
-myObj={first:"karthik",last:"gouda"
+// // //console.log("hello");
+// // //var a=10;
+// // //var b=20;
+// // //sum=a+b;
+// // //console.log(sum)
+// // //console.log(a==b)
+// // myObj.test="test"
+// // myObj={first:"karthik",last:"gouda"
 
+// // // }
+// // // myObj.first="john";
+// // // console.log(myObj)
+// function cookmaggie(water,maggie,pan){
+//     console.log("ypur maggie will be ready in 2 minutes "+maggie+"noodless with 5gm no fat present"
+//     +water+" glass water is added  glass of pure one "+
+//     pan+"the pan is used in clean one ")
 // }
-// myObj.first="john";
-// console.log(myObj)
+// cookmaggie(6,2,5)
