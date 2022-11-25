a7855543b37c8e5081a8434369572c97bbb6deea
karthik0788 <karthikgouda941@gmail.com>
2022-11-24 17:32:14 -0800

    this a

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-//function calculateTotalPrice(quantity, unitPrice) { 
-//	return quantity * unitPrice;
- //
-//}
-//console.log(calculateTotalPrice(3, "500"));
+function calculateTotalPrice(quantity, unitPrice) { 
+	return quantity * unitPrice;
+
+}
+console.log(calculateTotalPrice(3, "500"));
 let message:string="hello world"
 console.log(message);
+//var a=10;
+//console.log(a)
+    
